10e48d0f59873345fb5e83425e7fd440086bd645
Gustavo López Blandon <gustavo@kommit.co>
2016-09-25 09:15:51 -0500

    this.props.children in React and React Router

--- app/index.js
+++ app/index.js
@@ -1,51 +1,8 @@
-var USER_DATA = {
-  name: 'Gustavo A. Lopez',
-  username: 'galopezb',
-  image: 'https://avatars2.githubusercontent.com/u/11153775?v=3&u=d7a7a310de88fd38d409426f9c61244f43a81d6b&s=140'
-}
-
 var React = require('react');
 var ReactDOM = require('react-dom');
-
-var ProfilePic = React.createClass({
-  render: function () {
-    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
-  }
-});
-
-var ProfileLink = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <a href={'https://www.github.com/' + this.props.username}>
-          {this.props.username}
-        </a>
-      </div>
-    );
-  }
-});
-
-var ProfileName = React.createClass({
-  render: function() {
-    return (
-      <div>{this.props.name}</div>
-    );
-  }
-});
-
-var Avatar = React.createClass({
-  render: function() {
-    return (
-      <div>
-        <ProfilePic imageUrl={this.props.user.image} />
-        <ProfileName name={this.props.user.name} />
-        <ProfileLink username={this.props.user.username} />
-      </div>
-    );
-  }
-});
+var routes = require('./config/routes')
 
 ReactDOM.render(
-  <Avatar user={USER_DATA} />,
+  routes,
   document.getElementById('app')
 );
