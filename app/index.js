890372429c45efb504ebedd5df39ba43d2fb0426
Gustavo López Blandon <gustavo@kommit.co>
2016-09-24 19:09:20 -0500

    Implement component using Pure Functions, Props, and Nesting React Components in React

--- app/index.js
+++ app/index.js
@@ -1,15 +1,51 @@
+var USER_DATA = {
+  name: 'Gustavo A. Lopez',
+  username: 'galopezb',
+  image: 'https://avatars2.githubusercontent.com/u/11153775?v=3&u=d7a7a310de88fd38d409426f9c61244f43a81d6b&s=140'
+}
+
 var React = require('react');
 var ReactDOM = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
   render: function () {
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function() {
     return (
-      <div> Hello World </div>
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>
+          {this.props.username}
+        </a>
+      </div>
+    );
+  }
+});
+
+var ProfileName = React.createClass({
+  render: function() {
+    return (
+      <div>{this.props.name}</div>
+    );
+  }
+});
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
     );
   }
 });
 
 ReactDOM.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
